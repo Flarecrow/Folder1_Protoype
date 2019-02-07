f73880c34ae0a92dd6846c900b7eb01e9f2e327e
Flarecrow <46488915+Flarecrow@users.noreply.github.com>
2019-02-07 23:15:54 +0100

    Made Basic Vertical and Diagonal movement

---
+++ Source/Folder1_Protoype/MyPawn.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyPawn.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+
+// Sets default values
+AMyPawn::AMyPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+   
+    // Create a dummy root component we can attach things to.
+    RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+    // Create a visible object
+    OurVisibleComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("OurVisibleComponent"));
+ 
+    OurVisibleComponent->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void AMyPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	check(PlayerInputComponent);
+
+	InputComponent->BindAxis("MoveX", this, &AMyPawn::Move_XAxis);
+	InputComponent->BindAxis("MoveY", this, &AMyPawn::Move_YAxis);
+}
+
+void AMyPawn::Move_XAxis(float AxisValue)
+{
+	//CurrentVelocity.X = FMath::Clamp(AxisValue, -1.0f, 1.0f) * 100.0f;
+	AddActorLocalOffset(FVector(AxisValue * GetWorld()->DeltaTimeSeconds *walkSpeed, 0, 0), true);
+}
+void AMyPawn::Move_YAxis(float AxisValue)
+{
+	//CurrentVelocity.Y = FMath::Clamp(AxisValue, -1.0f, 1.0f) * 100.0f;
+	AddActorLocalOffset(FVector(0, AxisValue * GetWorld()->DeltaTimeSeconds *walkSpeed, 0), true);
+}
+
